f3c941ab8190e6a1295962031858fc7cb7932204
Weidi Zhang <weidiz999@yahoo.com>
2017-04-01 13:12:12 -0700

    Loader now determines which script to load depending on path, preparation for tradeHistory page

--- loader.js
+++ loader.js
@@ -1,5 +1,15 @@
+var path = window.location.pathname;
+var scriptUrl;
+
+if (path == "/exchange" || path == "/marginTrading") {
+	scriptUrl = "exchangePage.js";
+}
+else if (path == "/tradeHistory") {
+	scriptUrl = "historyPage.js";
+}
+
 var script = document.createElement("script");
-script.src = chrome.extension.getURL("main.js");
+script.src = chrome.extension.getURL(scriptUrl);
 (document.head || document.documentElement).appendChild(script);
 
 script.onload = function() {
